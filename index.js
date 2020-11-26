575582360618d90ca83bd20e67f9a82b676631ed
csr87 <74970184+csr87@users.noreply.github.com>
2020-11-26 16:19:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
-const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const BotName = 'CSR BOT☄'; 
+const instagram = 'https://instagram.com/sarangga87_'; 
+const telegram = 'https://t.me/caesaranggaaa'; 
+const kapanbotaktif = 'KETIKA CSR AKTIF'; 
+const youtube = 'https://www.youtube.com/-';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
